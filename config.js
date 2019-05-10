b6db31cf576f4190cf262ebf105e0b9c25f54793
kakisoft <kakisttab@gmail.com>
2019-05-11 04:14:02 +0900

    e

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'かきプレイグラウンド', // Required
   author: 'かきそふと', // Required
-  description: 'ほげー',
+  description: 'Netlifyたのしいー',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
